d15ee1dc81651ea7f73c84a749fe100941acc515
Tim Bakker <BakkerT@wigo4it.nl>
2021-04-01 14:04:54 +0200

    commit zonder gitignore

--- BerekeningSpecificator.Web/Entities/Periode.cs
+++ BerekeningSpecificator.Web/Entities/Periode.cs
@@ -8,6 +8,23 @@ namespace BerekeningSpecificator.Web.Entities
     {
         public DateTime Van { get; }
         public DateTime TotEnMet { get; }
+
+        [JsonConstructor]
+        public Periode(DateTime van, DateTime totEnMet)
+        {
+            if (van > totEnMet)
+            {
+                throw new ArgumentException($"{van} ligt na {totEnMet}");
+            }
+
+            Van = van;
+            TotEnMet = totEnMet;
+        }
+
+        public override string ToString()
+        {
+            return $"{Van:dd-MM-yyyy} t/m {TotEnMet:dd-MM-yyyy}";
+        }
     }
 }
 
--- BerekeningSpecificator.Web/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ BerekeningSpecificator.Web/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace BerekeningSpecificator.Web.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 1 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 2 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 3 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 4 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 5 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 6 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 7 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 8 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 9 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using BerekeningSpecificator.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\_Imports.razor"
+#line 10 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\_Imports.razor"
 using BerekeningSpecificator.Web.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using BerekeningSpecificator.Web.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "D:\repos\BerekeningSpecificator\BerekeningSpecificator.Web\Shared\SurveyPrompt.razor"
+#line 12 "D:\Repos\berekeningspecificator\BerekeningSpecificator.Web\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
